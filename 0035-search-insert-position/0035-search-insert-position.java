a6263aa5567b91a53bf1e3ade95848b311fc5a2e
SandeepKumarMamidipaka <sandeepkumarmamidipaka11@gmail.com>
2025-08-16 16:24:58 +0530

    Time: 0 ms (100%), Space: 43 MB (50.77%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,10 +2,9 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int i=0;
         int j=nums.length-1;
-        if(nums.length==1 && nums[0]>target){
-            return 0;
-        }
-        // else {return 1;}
+        // if(nums.length==1 && nums[0]>target){
+        //     return 0;
+        // }
         while(j>=i){
             int mid=(i+j)/2;
             if(nums[mid]==target){
@@ -20,17 +19,17 @@ class Solution {
             }
         }
         
-        i=0;j=i+1;
-        while(j!=nums.length){
-            if(nums[i]<target && nums[j]>target){
-                return j;
-            }
-            if(nums[i]>target){
-                return 0;
-            }
-            i++;
-            j++;
-        }
-        return nums.length;
+        // i=0;j=i+1;
+        // while(j!=nums.length){
+        //     if(nums[i]<target && nums[j]>target){
+        //         return j;
+        //     }
+        //     if(nums[i]>target){
+        //         return 0;
+        //     }
+        //     i++;
+        //     j++;
+        // }
+        return i;
     }
 }
